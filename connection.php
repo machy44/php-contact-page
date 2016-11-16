f383ccee5f87584eefcec29cbc67fffd233b3e7d
machy44 <machy44@gmail.com>
2016-11-16 02:14:57 +0100

    Delete connection.php

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-//connect to server
-$con  = mysql_connect("localhost","root","root");
-if(!$con){
-    die("Cannot to connect the database: " . mysql_error());
-}
-//connect to database
-$connect=mysql_select_db("php-contact-page",$con);
-?>

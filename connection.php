f472debe0b998b4d6af44f6625965cf60cc47da2
machy44 <machy44@gmail.com>
2016-11-16 03:05:04 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,18 +1,9 @@
 <?php
 //connect to server
-<<<<<<< HEAD
-$conn  = mysqli_connect("","","","");
+$conn  = mysqli_connect("localhost","root","root","php-contact-page");
 if(!$conn){
     die("Cannot to connect the database: " . mysql_error());
 }
 //connect to database
 ?>
-=======
-$con  = mysql_connect("","","");
-if(!$con){
-    die("Cannot to connect the database: " . mysql_error());
-}
-//connect to database
-$connect=mysql_select_db("php-contact-page",$con);
-?>
->>>>>>> origin/master
+
